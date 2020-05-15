2c10838e4914cfab780f9c00bb5f04caa49e67f3
agilly1989 <50060406+agilly1989@users.noreply.github.com>
2020-05-16 01:58:21 +1000

    Update moonlight_install.sh

--- moonlight_install.sh
+++ moonlight_install.sh
@@ -1,3 +1,6 @@
+sudo apt-get install git libopus0 libexpat1 libasound2 libudev1 libavahi-client3 libcurl4 libevdev2 libenet7 libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev cmake gcc g++ fakeroot debhelper
+sudo apt-get install libraspberrypi-dev
+
 git clone https://github.com/irtimmer/moonlight-embedded.git
 cd moonlight-embedded
 git submodule update --init
